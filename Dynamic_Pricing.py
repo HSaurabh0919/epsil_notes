706e6b7e9a5deb6a73c6ffb9f98c730b7649fc49
CCsaurabh <saurabhm2795@gmail.com>
2020-12-30 22:19:02 +0530

    Add files via upload

---
+++ Dynamic_Pricing.py
@@ -0,0 +1,5 @@
+###
+Dynamic pricing, also referred to as surge pricing, demand pricing, or time-based
+pricing is a pricing strategy in which businesses set flexible prices for
+products or services based on current market demands.
+ ###
